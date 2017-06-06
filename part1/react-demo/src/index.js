1d535e7131711999ab5530ff4a50878e630a05f6
amys <707804052@qq.com>
2017-06-06 09:48:53 +0800

    事件监听

--- part1/react-demo/src/index.js
+++ part1/react-demo/src/index.js
@@ -1,8 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Title extends Component {
+    handleClickOnTitle(e){
+        console.log(this)
+    }
+    render(){
+        return(
+            <h1
+                onClick={this.handleClickOnTitle}
+            >React小书</h1>
+        )
+    }
+}
+
+class Header extends React.Component {
+    render(){
+        return(
+            <div>
+                <Title/>
+                <h1>头部</h1>
+            </div>
+        )
+    }
+}
+
+class Index extends React.Component {
+    render(){
+        return(
+           <Header/>
+        )
+    }
+}
+
+class Dog extends Component {
+    bark(){
+        console.log('brak')
+    }
+    run(){
+        console.log('run')
+    }
+    render(){
+        return(
+            <div
+                onClick={() => {
+                    this.bark();
+                    this.run();
+                }}
+            >
+                DOG
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <Dog />,
+    document.getElementById('root')
+);
