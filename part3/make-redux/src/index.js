774e953dde74fc1aa9d287e720cb2fc2736f088f
amys <707804052@qq.com>
2017-06-07 09:32:31 +0800

    第三阶段dispatch

---
+++ part3/make-redux/src/index.js
@@ -0,0 +1,47 @@
+const appState = {
+    title: {
+        text: 'React.js 小书',
+        color: 'red'
+    },
+    content: {
+        text: 'Reat.js 小书内容',
+        color: 'blue'
+    }
+}
+
+renderApp(appState);
+
+function renderApp(appState) {
+    renderTitle(appState.title);
+    renderContent(appState.content);
+}
+
+function renderTitle(title) {
+    const titleDOM = document.getElementById('title');
+    titleDOM.innerHTML = title.text;
+    titleDOM.style.color = title.color;
+}
+
+function renderContent(content) {
+    const contentDOM = document.getElementById('content');
+    contentDOM.innerHTML = content.text;
+    contentDOM.style.color = content.color;
+}
+
+dispatch({
+    type: 'UPDATE_TITLE_TEXT',
+    text: '《React.js 小书》'
+})
+
+function dispatch(action) {
+    switch (action.type) {
+        case 'UPDATE_TITLE_TEXT':
+            appState.title.text = action.text;
+            break;
+        case 'UPDATE_TITLE_COLOR':
+            appState.title.color = action.color;
+            break;
+        default:
+            break;
+    }
+}
